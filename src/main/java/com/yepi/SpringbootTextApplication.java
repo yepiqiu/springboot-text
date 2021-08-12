976c7338ee6588f601a01609dc33b28f7ca04875
yepi.qiu <yepi.qiu@cloudwise.com>
2021-08-12 14:01:39 +0800

    text

---
+++ src/main/java/com/yepi/SpringbootTextApplication.java
@@ -0,0 +1,13 @@
+package com.yepi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTextApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yepi/SpringbootTextApplicationTests.java
@@ -0,0 +1,14 @@
+package com.yepi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTextApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
