768d8b63cc5d65e8867dcd2d60c011019eea4c8e
yepi.qiu <yepi.qiu@cloudwise.com>
2021-08-12 14:17:14 +0800

    innit

--- src/main/java/com/yepi/SpringbootTextApplication.java
+++ src/main/java/com/yepi/SpringbootTextApplication.java
@@ -2,12 +2,20 @@ package com.yepi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootTextApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootTextApplication.class, args);
     }
 
+
+    @RequestMapping("/get")
+    String test() {
+        return "hello,world";
+    }
 }
